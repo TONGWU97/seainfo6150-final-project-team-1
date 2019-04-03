3169092617701403174adbc41f425419106579c7
Yue <xiao.yue1@husky.neu.edu>
2019-04-03 16:50:48 -0700

    add

--- src/About/About.js
+++ src/About/About.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import style from './About.module.css';
 
 const About = () => (
-  <h1>About</h1>
+<div>
+
+  <div className = {style.about}>
+
+  </div>
+
+  <div className = {style.about_text}>
+    <strong><p>About us</p></strong>
+    <p>Vehicle Dealer, founded in March 2019, as a leading automobile Internet platform, is committed to providing consumers with high-quality automobile consumption and automobile life services, helping the automobile industry to flourish.</p>
+    <p>Covering more than 200 mainstream cities in the United States, our business always adheres to the direct selling mode of "no middleman to earn price difference". </p>
+    <p>We are promoting the transparency of car condition and price, which has played a revolutionary role in the rapid development of the industry.</p>
+  </div>
+
+</div>
 );
 
 export default About;
