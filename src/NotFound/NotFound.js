ea47c35aa3c87810f98f458121f751caa9250c91
Yue <xiao.yue1@husky.neu.edu>
2019-04-06 16:58:30 -0700

    add

--- src/NotFound/NotFound.js
+++ src/NotFound/NotFound.js
@@ -3,17 +3,18 @@ import style from './NotFound.module.css';
 
 const NotFound = () => (
 <div>
-  <div className = {style.notfound}>
+  <div className = {style.notfound_img}>
+    <img src = "./assets/img/notfound/question.png" />
+    <img src = "./assets/img/notfound/car.png" />
+  </div>
 
     <div className = {style.notfound_text}>
       <h1>404</h1>
       <p>404.That's an error.</p>
       <p>The request of URL was found on this sever.</p>
     </div>
-
-
   </div>
-</div>
+
 
 )
 
