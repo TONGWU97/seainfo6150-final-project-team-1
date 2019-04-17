b884e62890ac0ef069352c8fce87e564a178fbe6
Yuwei Chang <chang.yuwe@husky.neu.edu>
2019-04-17 11:16:06 -0700

    Update Home

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -13,16 +13,20 @@ class Home extends Component {
    
       
       <div>
-        <h1 className={styles.Title}>Home</h1>
-        <div className={styles.MoveCar}>
-          <img src="https://cdn.dribbble.com/users/2128392/screenshots/4316520/highway-5-take-3.gif" alt="Moving Car" />
-        </div>
-        <link href="./Home.module.css" rel="stylesheet" type="text/css" />
+      <div className={styles.Title}>
+        <div className={styles.Home}>
+          <h1 className={styles.Home}>Home</h1>
+          <link href="./Home.module.css" rel="stylesheet" type="text/css" />
+          </div>
+          <div className={styles.MovingCar}>
+            <img className={styles.MovingCar} src="http://ateliers.esapyrenees.fr/pom/malaurie/dear-krakow/img/gold-taxi.gif" alt="Moving Car" />
+          </div>
+          </div>
         <div className={styles.WelcomePage}>
           <img id={styles.HomeImage} src="https://images.unsplash.com/photo-1551952237-954a0e68786c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" alt="Welcome" />
           <div className={styles.Welcome}> Welcome </div>
         </div>
-        {viewedProducts.length === 0 ? <div></div>: <h2 className={styles.Title}>Viewed Products</h2>}
+        {viewedProducts.length === 0 ? <div></div>: <h2 className={styles.ViewedProducts}>Viewed Products</h2>}
 
         {/* start 5 most recently viewed products */}
         <ViewedProducts
